2f3d22a61b9fa74aabb60290b0e42ef6a7f10d87
llyyss1988 <llyyss88@163.com>
2018-12-28 15:59:16 +0800

    2
    2

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 sudo apt-get update
 echo `date`
+wget -q https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get install apt-transport-https -y
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.1 -y
 dotnet --version
+
+curl -OL https://github.com/google/protobuf/releases/download/v3.6.0/protoc-3.6.0-linux-x86_64.zip
+apt-get install unzip
+unzip protoc-3.6.0-linux-x86_64.zip -d protoc3
+sudo mv protoc3/bin/* /usr/local/bin/
+sudo mv protoc3/include/* /usr/local/include/
 protoc --version
-mkdir /home/c -p 
+
+sudo mkdir /home/c -p 
 for file in `ls | grep proto |grep -v grep`
 do
 echo $file
