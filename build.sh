6eed61fd54ae54347639721516b646342f74e23f
llyyss1988 <llyyss88@163.com>
2018-12-28 16:45:22 +0800

    q
    q

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ sudo mkdir /home/c -p
 for file in `ls | grep proto |grep -v grep`
 do
 echo $file
-protoc --proto_path=./ --csharp_out=/home/c  --csharp_opt=file_extension=.g.cs $file
+sudo protoc --proto_path=./ --csharp_out=/home/c  --csharp_opt=file_extension=.g.cs $file
 done
 ls /home/c
 echo 'eeeee'
