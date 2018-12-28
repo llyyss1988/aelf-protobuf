80ad6d08cdb73b5a7ddfa5bc70a589682349dd03
llyyss1988 <llyyss88@163.com>
2018-12-28 19:31:46 +0800

    eee
    eeee

--- build.sh
+++ build.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 sudo apt-get update
-dotnet --version
+
+
+old_path=`pwd`
+sudo mkdir /test && cd /test
+sudo curl -OL https://github.com/google/protobuf/releases/download/v3.6.0/protoc-3.6.0-linux-x86_64.zip
+sudo apt-get install unzip
+sudo unzip protoc-3.6.0-linux-x86_64.zip -d protoc3
+sudo mv protoc3/bin/* /usr/local/bin/
+sudo mv protoc3/include/* /usr/local/include/
+echo 'protoc-------'
 protoc --version
+echo 'dotnet-------'
+dotnet --version
+echo 'nuget--------'
 nuget
 
-sudo mkdir ~/c -p 
 
+
+cd $old_path
+sudo mkdir ~/c -p 
 for file in `ls | grep proto |grep -v grep`
 do
 echo $file
