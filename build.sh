722e16160314ec2001d243126aa32003b08d0b35
llyyss1988 <llyyss88@163.com>
2018-12-28 16:03:25 +0800

    3
    3

--- build.sh
+++ build.sh
@@ -19,6 +19,6 @@ sudo mkdir /home/c -p
 for file in `ls | grep proto |grep -v grep`
 do
 echo $file
-protoc --proto_path=./aelf-protobuf --csharp_out=/home/c  --csharp_opt=file_extension=.g.cs $file
+protoc --proto_path=../aelf-protobuf --csharp_out=/home/c  --csharp_opt=file_extension=.g.cs $file
 done
 ls /home/c
