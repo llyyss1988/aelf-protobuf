e8dd43920abe67a436a51cc09f30fc660f985016
llyyss1988 <llyyss88@163.com>
2018-12-28 17:04:52 +0800

    qa
    qa

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ sudo unzip protoc-3.6.0-linux-x86_64.zip -d protoc3
 sudo mv protoc3/bin/* /usr/local/bin/
 sudo mv protoc3/include/* /usr/local/include/
 protoc --version
+sudo curl -o /usr/local/bin/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+sudo alias nuget="mono /usr/local/bin/nuget.exe"
 
 cd $old_path
 echo `pwd`
@@ -21,3 +23,14 @@ sudo protoc --proto_path=./ --csharp_out=/home/c  --csharp_opt=file_extension=.g
 done
 ls /home/c
 echo 'eeeee'
+
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt install apt-transport-https
+sudo echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt install mono-devel -y
+echo '-------------------------------------'
+nuget
+
+
