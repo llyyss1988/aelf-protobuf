b22fac2633b89cc78e8ad2466f050dd87a38bad3
llyyss1988 <llyyss88@163.com>
2018-12-28 19:25:06 +0800

    22
    222

--- build.sh
+++ build.sh
@@ -4,4 +4,13 @@ dotnet --version
 protoc --version
 nuget
 
+sudo mkdir ~/c -p 
+
+for file in `ls | grep proto |grep -v grep`
+do
+echo $file
+sudo protoc --proto_path=./ --csharp_out=~/c  --csharp_opt=file_extension=.g.cs $file
+done
+
+ls ~/c/*
 
